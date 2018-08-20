754627f6c100e1da26f075940ab82a20a701af2a
Jiah <itisgia@gmail.com>
2018-08-21 11:51:34 +1200

    linking css file

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const http = require('http'); //http.creatServer()
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-var server = http.createServer(function (request, response ) { //1.send a request. 2. want soethig back (response) response is what our files gonna be
+
+var server = http.createServer(function(request, response){
+    console.log(`${request.method} request for ${request.url}`);
     var page;
-    if (request.url === "/") {
+
+    if(request.url === "/" || request.url === "home" || request.url === "index"){
         page = "home";
-    } else if (request.url === "/contact") {
-        page = "contact";
-    }else if (request.url ==="/about") {
-        page = "about";
+        fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url.match(/.css$/)){
+        var cssPath = path.join(__dirname, 'public', request.url);
+        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        fileStream.pipe(response);
     }
+    // else if (request.url === "/contact") {
+    //     page = "contact";
+    // }else if (request.url ==="/about") {
+    //     page = "about";
+    // }
 
-    response.writeHead(200, {'Content-Type':'text/html'}); //writing header informaton. text/plain can't run js.just html and css
-    // response.end('This is running from a server') // end of response what we want.
-    response.end(`
-        <html>
-            <head>
-                <title>Node Server</title>
-            </head>
-            <body>
-                <h1>${page}</h1>
-                <p>${request.url}</p>
-                <p>${request.method}</p>
-            </body>
-        </html>
-
-        `);
+    // response.writeHead(200, {'Content-Type':'text/html'}); //writing header informaton. text/plain can't run js.just html and css
+    // // response.end('This is running from a server') // end of response what we want.
+    // response.end(`
+    //     <html>
+    //         <head>
+    //             <title>Node Server</title>
+    //         </head>
+    //         <body>
+    //             <h1>${page}</h1>
+    //             <p>${request.url}</p>
+    //             <p>${request.method}</p>
+    //         </body>
+    //     </html>
+    //
+    //     `);
 }); //creat a server
 
 server.listen(5000); //i'm gonna run my server port3000 in vagrat server type :3000 behind the vagrant server address
