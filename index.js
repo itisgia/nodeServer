4c1f547e03a8bf13e3690bb0e86c9ec6d6b948e7
Jiah <itisgia@gmail.com>
2018-08-22 10:04:02 +1200

    getting image and mockaroo data

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const data = require('./data/products');
+const qs = require('querystring')
 
 
 var server = http.createServer(function(request, response){
     console.log(`${request.method} request for ${request.url}`);
     var page;
 
-    if(request.url === "/" || request.url === "home" || request.url === "index"){
-        page = "home";
-        fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
-            if(error){
-                console.log("error, something went wrong");
-            } else {
-                response.writeHead(200, {'Content-Type':'text/html'});
-                response.end(contents);
-            }
-        });
-    } else if(request.url.match(/.css$/)){
-        var cssPath = path.join(__dirname, 'public', request.url);
-        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
-        response.writeHead(200, {'Content-Type': 'text/css'});
-        fileStream.pipe(response);
+    if (request.method ==="GET") {
+        if(request.url === "/" || request.url === "home" || request.url === "index" || request.url === "contact" ){
+            page = "home";
+            fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
+                if(error){
+                    console.log("error, something went wrong");
+                } else {
+                    response.writeHead(200, {'Content-Type':'text/html'});
+                    response.end(contents);
+                }
+            });
+        } else if(request.url.match(/.css$/)){
+            var cssPath = path.join(__dirname, 'public', request.url);
+            var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            fileStream.pipe(response);
+        } else if(request.url.match(/.js$/)){
+            var javaPath = path.join(__dirname, 'public', request.url);
+            var fileStream = fs.createReadStream(javaPath, 'UTF-8');
+            response.writeHead(200, {'Content-Type': 'text/javascript'});
+            fileStream.pipe(response);
+        } else if(request.url.match(/.jpg$/)){
+    		var imagePath = path.join(__dirname, 'public', request.url);
+    		var imageStream = fs.createReadStream(imagePath);
+    		response.writeHead(200, {'Content-Type':'image/jpeg'});
+    		imageStream.pipe(response);
+    	} else if (request.url === '/allProducts') {
+            response.writeHead(200, {'Content-Type':'text/json'});
+            response.end(JSON.stringify(data));
+        } else if (request.url === '/inStock') {
+            //if i type this url,
+            instock(response);
+
+        } else if (request.url === '/outofStock') {
+            outofStock(response);
+        }
+    } else if (request.method === "POST") {
+        if (request.url === './formSubmit' ) {
+            // console.log(request);
+            // response.writeHead(200, {'Content-Type':'text/html'});
+            // response.end(request);
+            var body = '';
+            request.on('data', function (data) {
+                body += data; // eah time around it;s gonna give chunk of datas into body
+            })
+
+            request.on('end', function () {
+                var formData = qs.parse(body);
+                console.log(body);
+            })
+        }
+
     }
+
+
     // else if (request.url === "/contact") {
     //     page = "contact";
     // }else if (request.url ==="/about") {
@@ -48,3 +89,22 @@ var server = http.createServer(function(request, response){
 
 server.listen(5000); //i'm gonna run my server port3000 in vagrat server type :3000 behind the vagrant server address
 console.log("THIS SERVER IS RUNNING ON PORT 5000");
+
+
+function instock(response) {
+    //data ia json data
+    //data filter is kinda loopfillter out data.
+    //item is going to change whick iteration
+    var stock = data.filter(function (item) {
+        return item.inStock === true;
+        //retyribg all item whichis tryw
+    });
+    response.end(JSON.stringify(stock));
+}
+
+function outofStock(response) {
+    var stockOut = data.filter(function (check) {
+        return check.inStock === false;
+    });
+    response.end(JSON.stringify(stockOut));
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
-console.log('js has loaded');
+console.log('has loaded');
+
+$.ajax({
+     url:"http://192.168.33.10:5000/allProducts",
+     dataType: "json",
+     type: "GET",
+    success : function (products) {
+        console.log(products);
+    },
+    error: function (error) {
+        console.log(error);
+        console.log("SONETHING WENT WRONG");
+    }
+})
